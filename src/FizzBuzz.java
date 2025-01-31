565cbb15f47672372068ff25918dadee6cc61c66
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2025-01-31 10:03:33 +0100

    version class

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,7 +1,16 @@
-public record FizzBuzz(int num) {
+public class FizzBuzz {
+    private final int num;
+
+    public FizzBuzz(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
 
     public void fizzBuzz() {
-        for (int number = 1; number <= this.num(); number++) {
+        for (int number = 1; number <= this.getNum(); number++) {
             if (number % 3 != 0 && number % 5 != 0) {
                 System.out.println(number);
                 continue;
